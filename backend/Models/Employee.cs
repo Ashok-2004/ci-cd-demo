a7f96c22324498567fae8c8bc41ae3f1ce712da5
Ashok-2004 <manishmishra8970@gmail.com>
2026-06-28 18:54:28 +0530

    employee

--- backend/Models/Employee.cs
+++ backend/Models/Employee.cs
@@ -16,7 +16,7 @@ public class Employee
 {
     public int Id { get; set; }
 
-    public string FullName { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty
 
     public string Email { get; set; } = string.Empty;
 
